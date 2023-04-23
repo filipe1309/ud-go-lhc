772c22ab6697cb6560573b6e7f89947fe39dd56e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-04-23 17:47:14 -0300

    feat: add go going with go v3 exercises

---
+++ classes/sec-03-hands-on-exerrcises-v3/exercise1.go
@@ -0,0 +1,16 @@
+package main
+
+/**
+ * Print out a some text with emojis
+ * Print out a raw string literal
+ */
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World ✌️😀😘🤓👽👾🤖👻👺👹👿🤡👏👋👍👎👊👌👈")
+
+	fmt.Println(`This is a raw 
+		string 
+		literal`)
+}
